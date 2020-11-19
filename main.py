fcad59b6f08aa524f810b1eb6af9e44600cad92f
anaya5837 <72808504+anaya5837@users.noreply.github.com>
2020-11-19 12:59:01 -0500

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,3 @@
+temp = TemperatureUnit.FAHRENHEIT
+if temp > 60:
+    light.set_pixel_color(4, light.rgb(250, 0, 0))
---
+++ main.ts
@@ -0,0 +1,5 @@
+let temp = TemperatureUnit.Fahrenheit
+if (temp > 60) {
+    light.setPixelColor(4, light.rgb(250, 0, 0))
+}
+
