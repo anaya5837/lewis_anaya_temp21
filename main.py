37a7cffee77d7b64b0bf12a3c95a4c345b34b7fc
anaya5837 <72808504+anaya5837@users.noreply.github.com>
2020-11-19 13:15:24 -0500

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
-temp = TemperatureUnit.FAHRENHEIT
-if temp > 60:
-    light.set_pixel_color(4, light.rgb(250, 0, 0))
+temp = input.temperature(TemperatureUnit.FAHRENHEIT)
+while True:
+    print("Temperature: " + temp)
+    if temp > 60:
+        light.set_pixel_color(4, light.rgb(250, 0, 0))
+    else:
+        light.clear()
--- main.ts
+++ main.ts
@@ -1,5 +1,10 @@
-let temp = TemperatureUnit.Fahrenheit
-if (temp > 60) {
-    light.setPixelColor(4, light.rgb(250, 0, 0))
+let temp = input.temperature(TemperatureUnit.Fahrenheit)
+while (true) {
+    console.log("Temperature: " + temp)
+    if (temp > 60) {
+        light.setPixelColor(4, light.rgb(250, 0, 0))
+    } else {
+        light.clear()
+    }
+    
 }
-
