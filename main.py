2146f38f50be5fc478c344fc9e7b03c476e84a02
anaya5837 <72808504+anaya5837@users.noreply.github.com>
2020-11-24 09:45:06 -0500

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 temp = input.temperature(TemperatureUnit.FAHRENHEIT)
 while True:
     print("Temperature: " + temp)
-    if temp > 60:
-        light.set_pixel_color(4, light.rgb(250, 0, 0))
+    if temp > 70:
+        light.set_all(light.rgb(250, 0, 0))
+    elif temp < 70 and temp > 40:
+        light.set_all(light.rgb(0, 250, 0))
     else:
-        light.clear()
+        light.set_all(light.rgb(0, 0, 250))
--- main.ts
+++ main.ts
@@ -1,10 +1,12 @@
 let temp = input.temperature(TemperatureUnit.Fahrenheit)
 while (true) {
     console.log("Temperature: " + temp)
-    if (temp > 60) {
-        light.setPixelColor(4, light.rgb(250, 0, 0))
+    if (temp > 70) {
+        light.setAll(light.rgb(250, 0, 0))
+    } else if (temp < 70 && temp > 40) {
+        light.setAll(light.rgb(0, 250, 0))
     } else {
-        light.clear()
+        light.setAll(light.rgb(0, 0, 250))
     }
     
 }
